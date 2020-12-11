f3d78eb37058dcff62fd3060b9f853bace46376a
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2020-12-11 17:22:10 +0100

    Abbellimento- 0 fantasia oggi

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ Infine scrivi sulla pagina nomecognomecolorepreferito20 */
 var firstName = prompt('Inserisca il suo nome: '); // Dichiarazione variabile firstName, Richiesta del valore all'utente
 var surname = prompt('Inserisca il suo cognome: '); // Dichiarazione variabile surname, Richiesta del valore all'utente
 var favoriteColor = prompt('Inserisca il suo colore preferito: '); // Dichiarazione variabile favoriteColor, Richiesta del valore all'utente
-
 // Concatenazione delle variabili ed aggiunta della stringa 20, tutto trasformato in minuscolo
 document.getElementById('password').innerHTML =
 (firstName + surname + favoriteColor + '20').toLowerCase();
+
+document.getElementById('this').innerHTML = '&Egrave; la tua password generata.';
